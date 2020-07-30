313c4f51b3ba648cbac51d4b7a6c922ea0fa57a2
uladzislau.kaminski <uladzislau.kaminski@nordstrom.com>
2020-07-30 04:02:14 +0300

    add remove script

---
+++ remove-and-add.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm combined-json.sh
+rm generator-json.sh
+rm push-json.sh
+
+cd ../fakeit-combine-script/ && git pull
+cp * ../fakeit
+cd ../fakeit
+
+chmod +x combined-json.sh
+chmod +x generator-json.sh
+chmod +x push-json.sh
