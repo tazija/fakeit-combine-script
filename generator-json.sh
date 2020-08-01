8b42e5293adc16a600e82072312ee052b667b992
uladzislau.kaminski <uladzislau.kaminski@nordstrom.com>
2020-08-01 14:00:33 +0300

    update

--- generator-json.sh
+++ generator-json.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-folders=2
-records=2500
+folders=5
+records=25000
 for i in `seq 0 $folders`; do
     n=$((i * $records))
     sed "s/%customer_id%/${n}/g" ./models/customers_template.yaml > ./models/customers_generated.yaml
